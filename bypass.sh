5791293c5723e5a2084e84a6788509b13ba4ba8f
fineJune <1227769636@qq.com>
2024-07-17 20:11:36 +0800

    Update bypass.sh

--- bypass.sh
+++ bypass.sh
@@ -27,10 +27,10 @@ select opt in "${options[@]}"; do
 
             # Create Temporary User
             echo -e "${NC}创建账户"
-            read -p "输入电脑名称(默认为 'Apple'): " realName
-            realName="${realName:=Apple}"
-            read -p "输入账户名称(默认为 'Apple'): " username
-            username="${username:=Apple}"
+            read -p "输入电脑名称(默认为 'Macbook'): " realName
+            realName="${realName:=Macbook}"
+            read -p "输入账户名称(默认为 'User'): " username
+            username="${username:=User}"
             read -p "输入账号密码 (默认是 '1234'): " passw
             passw="${passw:=1234}"
 
@@ -40,7 +40,7 @@ select opt in "${options[@]}"; do
             dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username"
             dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" UserShell "/bin/zsh"
             dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" RealName "$realName"
-            dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" UniqueID "501"
+            dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" UniqueID "502"
             dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" PrimaryGroupID "20"
             mkdir "/Volumes/Data/Users/$username"
             dscl -f "$dscl_path" localhost -create "/Local/Default/Users/$username" NFSHomeDirectory "/Users/$username"
