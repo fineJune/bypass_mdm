3a680dcb2a251d0a043563c845611a5898c77476
darcycao <1227769636@qq.com>
2025-07-29 21:07:30 +0800

    Create bypass_local.sh

---
+++ bypass_local.sh
@@ -0,0 +1,4 @@
+echo "0.0.0.0 deviceenrollment.apple.com" >> /etc/hosts
+echo "0.0.0.0 mdmenrollment.apple.com" >> /etc/hosts
+echo "0.0.0.0 iprofiles.apple.com" >>  /etc/hosts
+echo -e "${GRN}成功屏蔽网络验证及MDM绕过..."
