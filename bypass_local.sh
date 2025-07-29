2bb828f7b6af0dce2db0a1ab2c0498101bbe6b89
darcycao <1227769636@qq.com>
2025-07-29 21:08:23 +0800

    Update bypass_local.sh

--- bypass_local.sh
+++ bypass_local.sh
@@ -2,3 +2,11 @@ echo "0.0.0.0 deviceenrollment.apple.com" >> /etc/hosts
 echo "0.0.0.0 mdmenrollment.apple.com" >> /etc/hosts
 echo "0.0.0.0 iprofiles.apple.com" >>  /etc/hosts
 echo -e "${GRN}成功屏蔽网络验证及MDM绕过..."
+touch /Volumes/Data/private/var/db/.AppleSetupDone
+rm -rf /var/db/ConfigurationProfiles/Settings/.cloudConfigHasActivationRecord
+rm -rf /var/db/ConfigurationProfiles/Settings/.cloudConfigRecordFound
+touch /var/db/ConfigurationProfiles/Settings/.cloudConfigProfileInstalled
+touch /var/db/ConfigurationProfiles/Settings/.cloudConfigRecordNotFound
+
+echo -e "${GRN}MDM 已成功绕过!${NC}"
+echo -e "${NC}请重启Mac....${NC}"
